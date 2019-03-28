85a61b07f15786ef9faa05906838af1c7669ebaa
Rodrigo Zanquini Lopes <rodrigo.zanquini.lopes@gmail.com>
2019-03-27 22:27:07 -0300

    Projeto criado

---
+++ src/main/java/com/zanquinilopes/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.zanquinilopes.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
