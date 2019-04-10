1467bb0d27b121f2413ac633ce59d21eb959cb24
Rodrigo Zanquini Lopes <rodrigo.zanquini.lopes@gmail.com>
2019-04-09 23:46:46 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/zanquinilopes/cursomc/domain/Categoria.java
+++ src/main/java/com/zanquinilopes/cursomc/domain/Categoria.java
@@ -1,11 +1,16 @@
 package com.zanquinilopes.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable{
@@ -15,6 +20,10 @@ public class Categoria implements Serializable{
 		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
 		private String nome;
+		
+		@JsonManagedReference
+		@ManyToMany(mappedBy="categorias")
+		private List<Produto> produtos = new ArrayList<>();
 		
 		public Categoria () {
 		}
@@ -39,6 +48,14 @@ public class Categoria implements Serializable{
 
 		public void setNome(String nome) {
 			this.nome = nome;
+		}
+		
+		public List<Produto> getProdutos() {
+			return produtos;
+		}
+
+		public void setProdutos(List<Produto> produtos) {
+			this.produtos = produtos;
 		}
 
 		@Override
@@ -65,6 +82,7 @@ public class Categoria implements Serializable{
 				return false;
 			return true;
 		}
+
 		
 		
 }
