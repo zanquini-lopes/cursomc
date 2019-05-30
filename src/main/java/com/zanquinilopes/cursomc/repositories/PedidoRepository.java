41ccf6db948e871300cce3bb6aefcee6ebb2c738
Rodrigo Zanquini Lopes <rodrigo.zanquini.lopes@gmail.com>
2019-05-29 22:02:14 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/zanquinilopes/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.zanquinilopes.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zanquinilopes.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+ 
+}
